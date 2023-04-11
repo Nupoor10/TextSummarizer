cb6e24b38b7c804bcee351b8129b6c6b6d691273
Nupoor Santosh Shetye <shetyenupoor10@gmail.com>
2023-04-11 23:37:08 +0530

    Added Upload File Option

--- app.py
+++ app.py
@@ -10,7 +10,12 @@ def index() :
 @app.route("/summary", methods=['GET', 'POST'])
 def summary() :
     if request.method == 'POST' :
-        text_content = request.form['text-content']
+        if 'file-input' in request.files:
+            # If a file was uploaded, read the contents of the file
+            text_content = request.files['file-input'].read().decode('utf-8')
+        else:
+            # If no file was uploaded, read the text from the textarea
+            text_content = request.form['text-content']
         original_text, summary_results, count_original, count_summary = final_text_summary(text_content)
 
     return render_template('summary.html', original = original_text,summary = summary_results, 
